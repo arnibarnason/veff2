a37e643c64cb31a9a9e1fc332c26f994b52d0830
Arni B <arnibarnason@gmail.com>
2015-01-19 15:39:58 +0000

    added var currColor with right color value to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
+var currColor = '#000000';
+
 $('#picker').colpick({
 	flat:true,
 	layout:'hex',
 	submit:0,
-	colorScheme: 'dark'
+	colorScheme: 'dark',
+	color: '000000',
+	onChange: function(hsb,hex,rgb,el,bySetColor) {
+		currColor = '#'+hex;
+	}
 });
+
+
+
+// $('#imageView').on("mouseover", function() {
+// $(this).css("background-color", currColor );
+// });
