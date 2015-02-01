62da29f6f334d170405a26ffc97e257a5b24f336
Arni B <arnibarnason@gmail.com>
2015-02-01 01:40:57 +0000

    alot working better than befor

--- js/scripts.js
+++ js/scripts.js
@@ -3,39 +3,76 @@ $( document ).ready(function() {
     var context = canvas.getContext("2d");
 
     var isDrawing = false;
+    var isSelectedTextBox = false;
+    var newTextBox;
+    var textBoxPoint;
+    var isSelectedObject = false;
+    var theSelectedObject;
 
     $("#imageView").mousedown(function(e) {
+   		if(newTextBox) {
+       		newTextBox.remove();
+    	}
     	var startPoint = new Point();
     	startPoint.x = e.pageX - this.offsetLeft;
     	startPoint.y = e.pageY - this.offsetTop;
     	var nextColor = drawing.nextColor;
+    	var boldness = drawing.nextWidth;
     	isDrawing = true;
+    	isSelectedTextBox = false;
+    	isSelectedObject = false;
     	// hægt að refactora, sjá fyrirlestur 2b
     	if(drawing.nextObject === "square") {
-    		drawing.shapes.push(new  Rect(startPoint, nextColor));
+    		drawing.shapes.push(new  Rect(startPoint, nextColor, boldness));
     	}
     	if(drawing.nextObject === "line") {
-    		drawing.shapes.push(new Line(startPoint, nextColor));
+    		drawing.shapes.push(new Line(startPoint, nextColor, boldness));
     	}
     	if(drawing.nextObject === "circle") {
-    		drawing.shapes.push(new Circle(startPoint, nextColor));
+    		drawing.shapes.push(new Circle(startPoint, nextColor, boldness));
     	}
     	if(drawing.nextObject === "pen") {
-    		drawing.shapes.push(new Pencil(startPoint, nextColor));
+    		drawing.shapes.push(new Pencil(startPoint, nextColor, boldness));
     	}
+    	if(drawing.nextObject === "text") {
+    		isSelectedTextBox = true;
+    		newTextBox = $("<input />");
+    		newTextBox.css("position", "absolute");
+    		newTextBox.css("top", startPoint.y);
+    		newTextBox.css("left", startPoint.x);
+    		$("#canvasParent").append(newTextBox);
+    		newTextBox.focus();
+    		isDrawing = false;
+    		textBoxPoint = startPoint;
+    	}
+    	if(drawing.nextObject === "selector") {
+    		isDrawing = false;
+    		for (var i = 0; i < drawing.shapes.length; ++i) {
+				if(drawing.shapes[i].isAtPoint(startPoint)) {
+					isSelectedObject = true;
+					theSelectedObject = i;
+					drawing.shapes[i].initLastChosenPoint(startPoint);
+					break;
+					// context.lineWidth = 0;
+    	// 			context.shadowBlur = 30;
+    	// 			context.shadowColor = "blue";
+    	// 			context.strokeRect(drawing.shapes[i].startPoint.x, drawing.shapes[i].startPoint.y, drawing.shapes[i].rWidth, drawing.shapes[i].rHeight);
+				}
+				else {
+					isSelectedObject = false;
+				}
+  		  	}
+  		}
     });
 
     $("#imageView").mousemove(function(e) {
     	if(isDrawing === true) {
-    		var obj = drawing.shapes[drawing.shapes.length - 1];
     		var currPoint = new Point();
-    		currPoint.x = e.pageX - this.offsetLeft;
+			currPoint.x = e.pageX - this.offsetLeft;
 			currPoint.y = e.pageY - this.offsetTop;
+    		var obj = drawing.shapes[drawing.shapes.length - 1];
 			obj.setEndPoint(currPoint);
-			drawing.drawAll();
-			if(drawing.nextObject === "square") {
-				obj.setHeightAndWidth();
-			}
+			obj.setHeightAndWidth();
 			if(drawing.nextObject === "circle") {
 				obj.setRadius();
 				obj.setCenterPoint();
@@ -43,12 +80,44 @@ $( document ).ready(function() {
 			if(drawing.nextObject === "pen") {
 				obj.addToDrawingArr(currPoint);
 			}
+			drawing.drawAll();
+    	}
+    	else if (isSelectedObject === true) {
+    		var currPoint = new Point();
+			currPoint.x = e.pageX - this.offsetLeft;
+			currPoint.y = e.pageY - this.offsetTop;
+    		var obj = drawing.shapes[theSelectedObject];
+    		obj.move(currPoint);
+    		drawing.drawAll();
     	}
     });
 
 	$("#imageView").mouseup(function(e) {
     	isDrawing = false;
+    	isSelectedObject = false;
 	});	
+	$("#imageView").mouseout(function(e) {
+    	isDrawing = false;
+    	isSelectedObject = false;
+	});
+
+	$(document).keypress(function(e) {
+	    if(isSelectedTextBox === true) {
+			if(e.which === 13) {
+				var text = newTextBox.val();
+				if(text.length === 0) {
+					newTextBox.remove();
+				}
+				else {
+					var obj = new Text(textBoxPoint, drawing.nextColor, text, drawing.nextFont);
+		            drawing.shapes.push(obj);
+		           	obj.setEndPoint();
+		            newTextBox.remove();
+		            isSelectedTextBox = false;				
+				}
+			}
+	    }
+	});
 
 	$('#picker').colpick({
 		flat:true,
@@ -61,8 +130,12 @@ $( document ).ready(function() {
 		}
 	});
 
-	$('input:radio').change( function() {
+	$("[name=options]").change( function() {
 		drawing.nextObject = this.value;
+	});
+
+	$("[name=boldness]").change( function() {
+		drawing.nextWidth = parseInt(this.value);
 	});
 
 	$("#undo").click(function() {
@@ -71,7 +144,6 @@ $( document ).ready(function() {
 			drawing.drawAll();
 			drawing.removedShapes.push(removedObj);
 		}
-
 	});
 
 	$("#redo").click(function() {
@@ -82,11 +154,17 @@ $( document ).ready(function() {
 		}
 	});
 
+	$("#save").click(function() {
+		localStorage.setItem(canvas, canvas.toDataURL());
+	});
+
 	var drawing = {
 		shapes: [],
 		removedShapes: [],
 		nextObject: "pen",
 		nextColor: "#000000",
+		nextWidth: 1,
+		nextFont: "Georgia",
 		drawAll: function drawAll() {
 			context.clearRect(0, 0, canvas.width, canvas.height);
 			for (var i = 0; i < this.shapes.length; ++i) {
@@ -101,49 +179,90 @@ $( document ).ready(function() {
 	};
 
 	var Shape = Base.extend( {
-		constructor: function(startPoint, color) {
+		constructor: function(startPoint, color, boldness) {
 			this.startPoint = startPoint;
 			this.endPoint = startPoint;
+			this.rWidth = 0;
+			this.rHeight = 0;
 			this.color = color;
+			this.boldness = boldness;
+			this.lastChosenPoint = startPoint;
 		},
 		startPoint: 0,
 		endPoint: 0,
+		rWidth: 0,
+		rHeight: 0,
 		color: "#000000",
+		boldness: "", 
+		lastChosenPoint: 0,
+
+		initLastChosenPoint: function initLastChosenPoint (point) {
+			this.lastChosenPoint = point;
+		},
+
+		setHeightAndWidth: function setHeightAndWidth() {
+			this.rWidth = this.endPoint.x - this.startPoint.x;
+			this.rHeight = this.endPoint.y - this.startPoint.y;
+		},
+
+		move: function move(toPoint) {
+			var xDiff = toPoint.x - this.lastChosenPoint.x;
+			var yDiff = toPoint.y - this.lastChosenPoint.y;
+			this.startPoint.x += xDiff;
+			this.startPoint.y += yDiff;
+			this.endPoint.x += xDiff;
+			this.endPoint.y += yDiff;
+			this.lastChosenPoint = toPoint;
+		},
 
 		setEndPoint: function(endPoint) {
 			this.endPoint = endPoint;
 		}, 
 
-		isAtPoint: function(x,y) {
-
+		isAtPoint: function(point) {
+		    // var canvasTemp = document.getElementById("imageView");
+    		// var contextTemp = canvasTemp.getContext("2d");
+    		// this.draw();
+    		// if(contextTemp.isPointInPath(point.x, point.y)){
+    		// 	return true;
+    		// }
+    		// else {
+    		// 	return false;
+    		// }
+    		
+    		if((this.startPoint.x < point.x && point.x < this.endPoint.x) || (this.startPoint.x > point.x && point.x > this.endPoint.x)) {
+	    		if((this.startPoint.y < point.y && point. y < this.endPoint.y) || (this.startPoint.y > point.y && point.y > this.endPoint.y)) {
+	    			return true;
+	    		}
+    		}
+    		return false;
 		}
 	});
 
 	var Rect = Shape.extend( {
-		constructor: function(startPoint, color) {
-			this.base(startPoint ,color);
+		constructor: function(startPoint, color, boldness) {
+			this.base(startPoint ,color, boldness);
 			this.rWidth = 0;
 			this.rHeight = 0;
 		},
 		rWidth: 0,
 		rHeight: 0,
-		setHeightAndWidth: function setHeightAndWidth() {
-			this.rWidth = this.endPoint.x - this.startPoint.x;
-			this.rHeight = this.endPoint.y - this.startPoint.y;
-		},
+
 		draw: function draw() {	
 			context.strokeStyle = this.color;
+			context.lineWidth = this.boldness;
 			context.strokeRect(this.startPoint.x, this.startPoint.y, this.rWidth, this.rHeight);
 		}
 	});
 
 	var Line = Shape.extend( {
-		constructor: function(startPoint, color) {
-			this.base(startPoint ,color);
+		constructor: function(startPoint, color, boldness) {
+			this.base(startPoint, color, boldness);
 			this.draw();	
 		},
 		draw: function draw() {	
 			context.strokeStyle = this.color;
+			context.lineWidth = this.boldness;
 			context.beginPath();
 			context.moveTo(this.startPoint.x, this.startPoint.y);
 			context.lineTo(this.endPoint.x, this.endPoint.y);
@@ -152,8 +271,8 @@ $( document ).ready(function() {
 	});
 
 	var Circle = Shape.extend( {
-		constructor: function(startPoint, color) {
-			this.base(startPoint, color);
+		constructor: function(startPoint, color, boldness) {
+			this.base(startPoint, color, boldness);
 		},
 		radius: 0,
 		centerPoint: 0,
@@ -169,25 +288,102 @@ $( document ).ready(function() {
 			cPoint.y = (this.endPoint.y + this.startPoint.y)/2;
 			this.centerPoint = cPoint;
 		},
+		move: function move(toPoint) {
+			var xDiff = toPoint.x - this.lastChosenPoint.x;
+			var yDiff = toPoint.y - this.lastChosenPoint.y;
+			this.centerPoint.x += xDiff;
+			this.centerPoint.y += yDiff;
+			this.startPoint.x += xDiff;
+			this.startPoint.y += yDiff;
+			this.endPoint.x += xDiff;
+			this.endPoint.y += yDiff;
+			this.lastChosenPoint = toPoint;
+		},
 		draw: function draw() {
 			context.strokeStyle = this.color;
+			context.lineWidth = this.boldness;
 			context.beginPath();
 			context.arc(this.centerPoint.x, this.centerPoint.y, this.radius, 0, Math.PI*2, false);
 			context.stroke();
 		}
 	});
 
-	var Pencil = Shape.extend( {
-		constructor: function(startPoint, color) {
-			this.base(startPoint, color);
+	var Pencil = Base.extend( {
+		constructor: function(startPoint, color, boldness) {
+			this.startPoint = startPoint;
+			this.endPoint = startPoint;
+			this.lastChosenPoint = startPoint;
+			this.color = color;
+			this.boldness = boldness;
 			this.drawingArr = [startPoint];
+			this.topLeftMost = startPoint;
+			this.bottomRightMost = startPoint;
 		},
+		startPoint: 0,
+		endPoint: 0,
+		lastChosenPoint: 0,
+		color: "#000000",
+		boldness: 0,
 		drawingArr: [],
 		addToDrawingArr: function addToDrawingArr(point) {
 			this.drawingArr.push(point);
+			// setTimeout(function () {
+			    
+  			// }, 2500);
+			// if(this.topLeftMost.x > point.x) {
+			// 	this.topLeftMost.x = point.x;
+			// }
+			// if(this.topLeftMost.y < point.y) {
+			// 	this.topLeftMost.y = point.y;
+			// }
+			// if(this.bottomRightMost.x < point.x) {
+			// 	this.bottomRightMost.x = point.x;
+			// }
+			// if(this.bottomRightMost.y > point.y) {
+			// 	this.bottomRightMost.y = point.y;
+			// }
+			this.endPoint = point;
 		},
+
+		topLeftMost: 0,
+		bottomRightMost: 0,
+		setHeightAndWidth: function setHeightAndWidth() {
+			this.rWidth = this.endPoint.x - this.startPoint.x;
+			this.rHeight = this.endPoint.y - this.startPoint.y;
+		},
+
+		initLastChosenPoint: function initLastChosenPoint (point) {
+			this.lastChosenPoint = point;
+		},
+
+		isAtPoint: function(point) {
+    		if((this.startPoint.x < point.x && point.x < this.endPoint.x) || (this.startPoint.x > point.x && point.x > this.endPoint.x)) {
+	    		if((this.startPoint.y < point.y && point. y < this.endPoint.y) || (this.startPoint.y > point.y && point.y > this.endPoint.y)) {
+	    			return true;
+	    		}
+    		}
+    		return false;
+		},
+
+		setEndPoint: function(endPoint) {
+			this.endPoint = endPoint;
+		}, 
+
+		move: function move(toPoint) {
+			var xDiff = toPoint.x - this.lastChosenPoint.x;
+			var yDiff = toPoint.y - this.lastChosenPoint.y;
+			this.lastChosenPoint = toPoint;
+			for(var i = 0; i < this.drawingArr.length; i++)
+			{
+				this.drawingArr[i].x += xDiff;
+				this.drawingArr[i].y += yDiff;
+			}
+
+		},
+
 		draw: function draw() {
 			context.strokeStyle = this.color;
+			context.lineWidth = this.boldness;
 			context.beginPath();
 			context.moveTo(this.startPoint.x, this.startPoint.y);
 			for(var i = 0; i < this.drawingArr.length; i++)
@@ -195,15 +391,62 @@ $( document ).ready(function() {
 				context.lineTo(this.drawingArr[i].x,this.drawingArr[i].y);
 			}
 			context.stroke();
-			context.closePath();
+			//context.closePath();
 		}
 	});
 
-
-
-	//visual test to be removed
-	// $('#imageView').on("mouseover", function() {
-	//  $(this).css("background-color", drawing.nextColor );
-	//  console.log(drawing.nextObject);
-	// });
+	var Text = Base.extend( {
+		constructor: function(startPoint, color, text, font) {
+			this.startPoint = startPoint;
+			this.endPoint = startPoint;
+			this.lastChosenPoint = startPoint;
+			this.color = color;
+			this.string = text;
+			this.inputBox = {};
+			this.font = font;
+			this.widthOfText = 0;
+			this.draw();
+		},
+		startPoint: 0,
+		endPoint: 0,
+		lastChosenPoint: 0,
+		color: 0,
+		inputBox: {},
+		string: "",
+		font: "",
+		widthOfText: 0,
+		initLastChosenPoint: function initLastChosenPoint (point) {
+			this.lastChosenPoint = point;
+		},
+		move: function move(toPoint) {
+			var xDiff = toPoint.x - this.lastChosenPoint.x;
+			var yDiff = toPoint.y - this.lastChosenPoint.y;
+			this.startPoint.x += xDiff;
+			this.startPoint.y += yDiff;
+			this.endPoint.x += xDiff;
+			this.endPoint.y += yDiff;
+			this.lastChosenPoint = toPoint;
+		},
+		setEndPoint: function() {
+			var yD = 50;
+			var xD = parseInt(this.widthOfText);
+			var newEndPoint = {x: this.startPoint.x + xD, y: this.startPoint.y + 30};
+			this.endPoint = newEndPoint;
+		}, 
+		isAtPoint: function(point) {
+    		if((this.startPoint.x < point.x && point.x < this.endPoint.x) || (this.startPoint.x > point.x && point.x > this.endPoint.x)) {
+	    		if((this.startPoint.y < point.y && point. y < this.endPoint.y) || (this.startPoint.y > point.y && point.y > this.endPoint.y)) {
+	    			return true;
+	    		}
+    		}
+    		return false;
+		},
+		draw: function draw() {
+			context.strokeStyle = this.color;
+			context.font = "20px " + this.font;
+			context.lineWidth = 1;
+			this.widthOfText = context.measureText(this.string).width;
+			context.strokeText(this.string, this.startPoint.x, this.startPoint.y + 15);
+   		}
+	})
 });
